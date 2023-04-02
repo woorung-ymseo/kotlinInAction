69a28c8f80dea779c5842e0f2be9fe9e20bb20d2
eric-seo <eric.seo@musinsa.com>
2023-04-02 16:34:38 +0900

    [kotlin in action] 1.1 코틀린 맛보기

---
+++ kotlin/src/chapter1/Person.kt
@@ -0,0 +1,20 @@
+package chapter1
+
+// 데이터 클래스
+data class Person(val name: String,
+                  val age: Int? = null)  // Int? < nullable default null
+
+// 최상위 main
+fun main(args: Array<String>) {
+    val persons = listOf(
+            Person("영희"),
+            Person("민수", 25),
+            Person("철수", age = 29) // 이름 붙인 파라미터
+    )
+
+    // it을 사용하면 파라미터 명을 정의하지 않아도 람다식의 유일한 인자를 사용할 수 있다.
+    val oldest = persons.maxByOrNull { it.age ?: 0 } // 책에는 maxBy 였으나 deprecated 여서 maxByOrNull 로 사용
+    val youngest = persons.minByOrNull { it.age ?: 0 } // 책에는 maxBy 였으나 deprecated 여서 maxByOrNull 로 사용
+    println("나이가 가장 적은 사람 : $youngest")
+    println("나이가 가장 많은 사람 : $oldest")
+}
