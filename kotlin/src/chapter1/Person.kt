4c40bd92e5c8fe403b8ade095a33a9f258b3d3d6
eric-seo <eric.seo@musinsa.com>
2023-11-22 13:00:10 +0900

    ㄴㄴ

--- kotlin/src/chapter1/Person.kt
+++ kotlin/src/chapter1/Person.kt
@@ -13,8 +13,9 @@ fun main(args: Array<String>) {
     )
 
     // it을 사용하면 파라미터 명을 정의하지 않아도 람다식의 유일한 인자를 사용할 수 있다.
-    val oldest = persons.maxByOrNull { it.age ?: 0 } // 책에는 maxBy 였으나 deprecated 여서 maxByOrNull 로 사용
-    val youngest = persons.minByOrNull { it.age ?: 0 } // 책에는 maxBy 였으나 deprecated 여서 maxByOrNull 로 사용
+    // https://kotlinlang.org/api/latest/jvm/stdlib/kotlin.collections/max-by.html
+    val oldest = persons.maxByOrNull { it.age ?: 0 } // 책에는 maxBy 였으나 deprecated 여서 maxByOrNull 로 사용 -> 1.7부터는 다시 생긴듯?
+    val youngest = persons.minByOrNull { it.age ?: 0 } // 책에는 maxBy 였으나 deprecated 여서 maxByOrNull 로 사용 -> 1.7부터는 다시 생긴듯?
     println("나이가 가장 적은 사람 : $youngest")
     println("나이가 가장 많은 사람 : $oldest")
 }
